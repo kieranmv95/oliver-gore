a5bfbbcbefafa6167833955708ed88a6fd38e008
kieranmv95 <kieranmv@gmail.com>
2019-03-03 20:04:56 +0000

    changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Oliver Gore`,
+    description: `Oliver gore's personal website.`,
+    author: `@kieranmv95`,
     siteUrl: `https://www.olivergore.co.uk`
   },
   plugins: [
